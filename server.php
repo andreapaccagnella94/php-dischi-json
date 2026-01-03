c08d019ba70a4b32029254f8c2e7b601846cb631
andreapaccagnella94 <andreapacca94@live.it>
2026-01-03 18:06:58 +0100

    Refactor server-side validation and data handling for album submission

--- server.php
+++ server.php
@@ -1,38 +1,51 @@
 <?php
+// aggiungo validazione lato server
 
-// recupero i dati già decodificati per leggerli in php
-require_once "./functions.php";
-$dischi = getDischi();
-// var_dump($dischi);
+if ($_POST["title"] && $_POST["artist"] && $_POST["year"] && $_POST["genre"] && $_POST["coverUrl"]) {
 
-// recupero i dati che mi arrivano in POST dal form (test personale per vedere se ho capito)
-// $title_new = $_POST["title"];
-//echo $title_new;
 
-// mi creo un variabile dopo andrò ad mettere tutti le nuove informazioni
-$album_number = count($dischi);
+    // recupero i dati già decodificati per leggerli in php
+    require_once "./functions.php";
+    $dischi = getDischi();
+    // var_dump($dischi);
 
-$album_new = [
-    "id" => $album_number + 1,
-    "title" => $_POST["title"],
-    "artist" => $_POST["artist"],
-    "year" => $_POST["year"],
-    "genre" => $_POST["genre"],
-    "coverUrl" => $_POST["coverUrl"]
-];
+    // recupero i dati che mi arrivano in POST dal form (test personale per vedere se ho capito)
+    // $title_new = $_POST["title"];
+    //echo $title_new;
 
-// var_dump($album_new);
+    // mi creo un variabile dopo andrò ad mettere tutti le nuove informazioni
+    $albums_number = count($dischi);
+    $title = $_POST["title"];
+    $artist = $_POST["artist"];
+    $year = $_POST["year"];
+    $genre = $_POST["genre"];
+    $coverUrl = $_POST["coverUrl"];
 
-// aggiungo la variabile nei dati iniziali
-$dischi[] = $album_new;
-// var_dump($dischi);
+    $album_new = [
+        "id" => $albums_number + 1,
+        "title" => $title,
+        "artist" => $artist,
+        "year" => $year,
+        "genre" => $genre,
+        "coverUrl" => $coverUrl
+    ];
 
-// riconverto la nuova struttura dati in stringa json
-$json_text_update = json_encode($dischi);
-// echo $json_text_update;
+    // var_dump($album_new);
 
-// sovrascrivo il contenuto del file aggiornato
-file_put_contents("./dischi.json", $json_text_update);
+    // aggiungo la variabile nei dati iniziali
+    $dischi[] = $album_new;
+    // var_dump($dischi);
 
-// reindirizzo l'utente all'index.php
-header("Location: ./index.php");
+    // riconverto la nuova struttura dati in stringa json
+    $json_text_update = json_encode($dischi);
+    // echo $json_text_update;
+
+    // sovrascrivo il contenuto del file aggiornato
+    file_put_contents("./dischi.json", $json_text_update);
+
+    // reindirizzo l'utente all'index.php
+    header("Location: ./index.php");
+} else {
+    // reindirizzo l'utente all'index.php
+    header("Location: ./index.php");
+}
